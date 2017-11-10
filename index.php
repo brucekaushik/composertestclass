cd9eeba493d43f3934bcd759ff29662058bb0a73
kaushik laptop <kaushik.srinivas@gmail.com>
2017-11-11 00:51:28 +0530

    all contents

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+require __DIR__ . '/vendor/autoload.php';
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->addWarning('Foo');
+
+$k = new Kaushik\Test;
+echo $k;
---
+++ src/Test.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace Kaushik;
+
+class Test 
+{
+	public function __toString(){
+		return "hello from Kaushik\Test";
+	}
+}
